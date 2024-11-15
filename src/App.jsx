ea7fbb1be02154e864027c4a27f595c28309e0e4
Ksingh79798 <118569392+Ksingh79798@users.noreply.github.com>
2024-11-16 02:50:19 +0530

    L-15 Routing, Outlet, Children routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Body from "./components/Body";
+import Profile from "./components/Profile";
+
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold ">Hello world!</h1>
-      </div>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
