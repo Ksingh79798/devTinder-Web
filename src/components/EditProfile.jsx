510347b8b9eb0fe6266c6f112bbcf7bb1715fc16
Ksingh79798 <118569392+Ksingh79798@users.noreply.github.com>
2024-12-07 03:18:33 +0530

    L-19 signUp features with Login

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -13,12 +13,12 @@ const EditProfile = ({ user }) => {
 
   const [firstName, setFirstName] = useState(user.firstName);
   const [lastName, setLastName] = useState(user.lastName);
-  const [age, setAge] = useState(user.age);
+  const [age, setAge] = useState(user.age || "");
   const [gender, setGender] = useState(user.gender);
   const [about, setAbout] = useState(user.about);
   const [photoUrl, setPhotoUrl] = useState(user.photoUrl);
   const [error, setError] = useState("");
-  const [showToast, setShowToast] = useState("false");
+  const [showToast, setShowToast] = useState(false);
 
   const saveProfile = async () => {
     try {
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 import axios from "axios";
 import { useDispatch } from "react-redux";
@@ -5,8 +6,11 @@ import { addUser } from "../utils/userSlice";
 import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 const Login = () => {
-  const [emailId, setEmailID] = useState("Neha@gmail.com");
-  const [password, setPassword] = useState("Neha@123");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [emailId, setEmailID] = useState("@gmail.com");
+  const [password, setPassword] = useState("@123");
+  const [isLoginForm, setIsLoginForm] = useState(true);
   const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -29,17 +33,63 @@ const Login = () => {
       console.error(err);
     }
   };
+
+  const handleSignUp = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        { firstName, lastName, emailId, password },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data.data));
+      return navigate("/profile");
+    } catch (err) {
+      // handle error case
+    }
+  };
   return (
     <>
       <div className="card bg-base-100 w-96 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title justify-center">Login</h2>
+          <h2 className="card-title justify-center">
+            {isLoginForm ? "Login" : "Signup"}
+          </h2>
 
-          {/* Email Id */}
           <div>
+            {!isLoginForm && (
+              <>
+                <label className="form-control w-full max-w-xs">
+                  <div className="label">
+                    <span className="label-text">First Name :</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={firstName}
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </label>
+
+                <label className="form-control w-full max-w-xs">
+                  <div className="label">
+                    <span className="label-text">Last Name :</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={lastName}
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </label>
+              </>
+            )}
+
+            {/* Email Id */}
             <label className="form-control w-full max-w-xs">
               <div className="label">
-                <span className="label-text">Email ID {emailId}</span>
+                <span className="label-text">Email Id :</span>
               </div>
               <input
                 type="text"
@@ -55,7 +105,7 @@ const Login = () => {
           <div>
             <label className="form-control w-full max-w-xs">
               <div className="label">
-                <span className="label-text">Password</span>
+                <span className="label-text">Password :</span>
               </div>
               <input
                 type="text"
@@ -66,12 +116,23 @@ const Login = () => {
               />
             </label>
           </div>
-          <div className="card-actions justify-center">
-            <p className="text-red-500 ">{error}</p>
-            <button className="btn btn-primary" onClick={handleLogin}>
-              Login
+          <p className="text-red-500 ">{error}</p>
+          <div className="card-actions justify-center m-2">
+            <button
+              className="btn btn-primary"
+              onClick={isLoginForm ? handleLogin : handleSignUp}
+            >
+              {isLoginForm ? "Login" : "Signup"}
             </button>
           </div>
+          <p
+            className="m-auto py-2"
+            onClick={() => setIsLoginForm((value) => !value)}
+          >
+            {isLoginForm
+              ? "New User? SignUp here"
+              : "Existing User? Login Here"}
+          </p>
         </div>
       </div>
     </>
